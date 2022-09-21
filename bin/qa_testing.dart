f29efb17db48f040d51fd912d39789d708729067
Aidar Maratbekov <aidarmaratbekov@MacBook-Pro-Aidar.local>
2022-09-21 20:34:25 +0600

    init projects

---
+++ bin/qa_testing.dart
@@ -0,0 +1,5 @@
+import 'package:qa_testing/qa_testing.dart' as qa_testing;
+
+void main(List<String> arguments) {
+  print('Hello world: ${qa_testing.calculate()}!');
+}
---
+++ test/qa_testing_test.dart
@@ -0,0 +1,8 @@
+import 'package:qa_testing/qa_testing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
